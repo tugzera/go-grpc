52d36aa94ea924d6c655dba36b1e9901ddb46a63
tugzera <damascenobdm@gmail.com>
2021-09-27 20:49:24 -0300

    feat: add client streaming

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"time"
 
 	"github.com/tugzera/go-grpc/pb"
 	"google.golang.org/grpc"
@@ -21,7 +22,8 @@ func main() {
 	client := pb.NewUserServiceClient(connection)
 
 	// AddUser(client)
-	AddUserVerbose(client)
+	// AddUserVerbose(client)
+	AddUsers(client)
 
 }
 
@@ -62,4 +64,53 @@ func AddUserVerbose(client pb.UserServiceClient) {
 		}
 		fmt.Println("Status: ", stream.Status)
 	}
+}
+
+func AddUsers(client pb.UserServiceClient) {
+	reqs := []*pb.User{
+		&pb.User{
+			Id:    "123",
+			Name:  "Bruno",
+			Email: "damascenobdm@gmail.com",
+		},
+		&pb.User{
+			Id:    "124",
+			Name:  "Jose",
+			Email: "jose@gmail.com",
+		},
+		&pb.User{
+			Id:    "125",
+			Name:  "Leo",
+			Email: "leo@gmail.com",
+		},
+		&pb.User{
+			Id:    "126",
+			Name:  "Alice",
+			Email: "alice@gmail.com",
+		},
+		&pb.User{
+			Id:    "127",
+			Name:  "Iza",
+			Email: "iza@gmail.com",
+		},
+	}
+
+	stream, err := client.AddUsers(context.Background())
+	if err != nil {
+		log.Fatalf("Error sending stream %e", err)
+	}
+
+	for _, req := range reqs {
+		stream.Send(req)
+		time.Sleep(time.Second * 3)
+	}
+
+	res, err := stream.CloseAndRecv()
+
+	if err != nil {
+		log.Fatalf("Error receiving response stream %e", err)
+	}
+
+	fmt.Println(res)
+
 }
--- services/user.go
+++ services/user.go
@@ -3,6 +3,8 @@ package services
 import (
 	"context"
 	"fmt"
+	"io"
+	"log"
 	"time"
 
 	"github.com/tugzera/go-grpc/pb"
@@ -64,4 +66,28 @@ func (*UserService) AddUserVerbose(req *pb.User, stream pb.UserService_AddUserVe
 	time.Sleep(time.Second * 3)
 
 	return nil
+}
+
+func (*UserService) AddUsers(stream pb.UserService_AddUsersServer) error {
+	users := []*pb.User{}
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(&pb.Users{
+				User: users,
+			})
+		}
+		if err != nil {
+			log.Fatalf("Could not receive stream message: %e", err)
+		}
+
+		users = append(users, &pb.User{
+			Id:    req.GetId(),
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		})
+
+		fmt.Println("Adding", req.GetName())
+	}
 }
